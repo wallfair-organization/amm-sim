653d30e981bc6810937a183e249d4b400fbe734f
Sebastian <sebastian.gajek@gmail.com>
2021-05-14 15:07:18 +0200

    corrected arithmetic in create_event()

--- cpmm.py
+++ cpmm.py
@@ -21,26 +21,33 @@ buy_price_yes = 0
 sell_price_yes = 0
 buy_price_no = 0
 sell_price_no = 0
-history = [] #array to keep track of buy/sell activity; structure buy/sell, n/y amount, token_buy_price_old, token_buy_price_new, returned tokens, lp_yes, lp_no, _lp_token
+history = [("activity", "type" "amount", "token_buy_price_old", "token_buy_price_new", "returned tokens", "lp_yes", "lp_no", "lp_token")]
 
 def create_event(liquidity):
 	add_liquidity(liquidity)
 
 def add_liquidity(amount):
 	global lp_token 
-	lp_token = lp_token + amount
 	global lp_yes 
-	lp_yes = lp_yes + amount
 	global lp_no 
-	lp_no = lp_no + amount
 	global buy_price_yes 
-	buy_price_yes = buy_price_yes + amount //  lp_yes + lp_no
 	global sell_price_yes 
-	sell_price_yes = sell_price_yes + amount //  lp_yes + lp_no
 	global buy_price_no
-	buy_price_no = buy_price_no + amount //  lp_yes + lp_no
 	global sell_price_no
-	sell_price_no = sell_price_no + amount //  lp_yes + lp_no
+
+	lp_token = lp_token + amount
+	lp_yes = lp_yes + amount
+	lp_no = lp_no + amount
+	buy_price_yes_old = buy_price_yes
+	buy_price_yes = (buy_price_yes + amount) /  (lp_yes + lp_no)
+	sell_price_yes = (sell_price_yes + amount) /  (lp_yes + lp_no)
+	buy_price_no = (buy_price_no + amount) /  (lp_yes + lp_no)
+	
+	sell_price_no = (sell_price_no + amount) / (lp_yes + lp_no)
+	
+	global history
+	entry = ("add", "liquidity", amount, buy_price_yes_old,  buy_price_yes, "0", lp_yes, lp_no, lp_token)
+	history.append(entry)
 
 
 
@@ -48,6 +55,8 @@ def add_liquidity(amount):
 	
 
 def buy_token(type, amount): #yes=1 | no = 0
+	
+	global history
 	if type:
 		
 		lp_yes += amount
@@ -57,8 +66,9 @@ def buy_token(type, amount): #yes=1 | no = 0
 		tokens_return = amount - x
 		buy_price_yes_old = buy_price_yes
 		buy_price_yes = amount / (amount - x)
-		#buy/sell, no/yes, amount, token_buy_price_old, token_buy_price_new, returned tokens, lp_yes, lp_no, _lp_token
-		history.append("buy", "yes", amount, buy_price_yes_old,  buy_price_yes, tokens_return, lp_yes, lp_no, lp_token)
+		
+		entry = ("buy", "yes", amount, buy_price_yes_old,  buy_price_yes, tokens_return, lp_yes, lp_no, lp_token)
+		history.append(entry)
 
 	else:
 		lp_yes += amount
@@ -68,8 +78,9 @@ def buy_token(type, amount): #yes=1 | no = 0
 		tokens_return = amount - x
 		buy_price_no_old = buy_price_no
 		buy_price_no = amount / (amount - x)
-		#buy/sell, no/yes, amount, token_buy_price_old, token_buy_price_new, returned tokens, lp_yes, lp_no, _lp_token
-		history.append("buy", "no", amount, buy_price_no_old,  buy_price_no, tokens_return, lp_yes, lp_no, lp_token)
+		
+		entry = ("buy", "no", amount, buy_price_no_old,  buy_price_no, tokens_return, lp_yes, lp_no, lp_token)
+		history.append(entry)
 
 
 		
